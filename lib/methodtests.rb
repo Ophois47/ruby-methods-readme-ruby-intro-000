775539c6f088ebccd4903fd6e24e2b0438a50671
David Petnick <dpetnick89@gmail.com>
2020-05-12 13:48:59 +0000

    Automatically backed up by Learn

---
+++ lib/methodtests.rb
@@ -0,0 +1,21 @@
+def my_name
+  puts "Dave"
+end
+
+def my_name_ten_times
+  phrase = "David"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
+
+my_name
+my_name
+my_name_ten_times
